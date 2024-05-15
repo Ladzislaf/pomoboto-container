9013c7f968b97e7cdebde5714f12fc6813357d05
ladzislaf <ladzislaf@gmail.com>
2024-05-15 15:17:57 +0300

    add cronjob logs

--- cron.js
+++ cron.js
@@ -9,15 +9,21 @@ const cronJob = new CronJob(
 		users.forEach(async (user) => {
 			const { todayStreak, dayGoal, includeWeekends } = user;
 			await db.updateUserSettings(user.id, 'todayStreak', 0);
+			console.log(`[CronJob] User ${user.id}: todayStreak = ${todayStreak} | dayGoal = ${dayGoal}.`);
 			if (todayStreak < dayGoal) {
+				console.log(`[CronJob] User ${user.id}: todayStreak is less than dayGoal.`);
 				const yesterday = new Date();
 				yesterday.setDate(yesterday.getDate() - 1);
-				console.log('CronJob yesterday date:', yesterday.toUTCString());
+				console.log('[CronJob] Yesterday date to check streak:', yesterday.toUTCString());
 				if (yesterday.getDay() === 6 || yesterday.getDay() === 0) {
+					console.log(`[CronJob] Yesterday was weekend. yesterday.getDay(): ${yesterday.getDay()}. (0 or 6)`);
 					if (includeWeekends) {
+						console.log(`[CronJob] User ${user.id}: includeWeekends = true => reseting currentDayStreak.`);
 						await db.updateUserSettings(user.id, 'currentDayStreak', 0);
 					}
 				} else {
+					console.log(`[CronJob] Yesterday wasn't weekend. yesterday.getDay(): ${yesterday.getDay()}. (1-5)`);
+					console.log(`[CronJob] User ${user.id}: reseting currentDayStreak.`);
 					await db.updateUserSettings(user.id, 'currentDayStreak', 0);
 				}
 			}
