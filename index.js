2a8f0f7166933bab89b30e3cdf832c6e42030fea
ladzislaf <ladzislaf@gmail.com>
2024-05-21 21:52:36 +0300

    add cancel focus, skip break commands

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-import { Telegraf, Markup, Scenes, session } from 'telegraf';
+import { Telegraf, Scenes, session } from 'telegraf';
 import streakJob from './cron.js';
 import dotenv from 'dotenv';
 import db from './db.js';
 import textSettingScene from './scenes/textSettingScene.js';
 import weekendsScene from './scenes/weekendsScene.js';
+import { botCommands, showMenuKeyboard, setFocusInterval, setfocusTimeout, changeSettingAction } from './utils.js';
 
 dotenv.config();
 streakJob.start();
@@ -14,21 +15,43 @@ const stage = new Scenes.Stage([textSettingScene, weekendsScene]);
 bot.use(session());
 bot.use(stage.middleware());
 
-bot.telegram.setMyCommands([
-	{
-		command: '/start',
-		description: 'Open the bot menu',
-	},
-	{
-		command: '/playlist',
-		description: 'Spotify playlist',
-	},
-]);
+bot.telegram.setMyCommands(botCommands);
 
 bot.start(async (ctx) => {
+	console.log('[Start] first name:', ctx.from.first_name);
+	console.log('[Start] last name:', ctx.from.last_name);
+	console.log('[Start] username:', ctx.from.username);
 	await ctx.deleteMessage();
 	await showMenuKeyboard(ctx);
-	await db.createNewUser(ctx.from.id);
+	return db.createNewUser(ctx.from.id);
+});
+
+bot.command('cancel', async (ctx) => {
+	await ctx.deleteMessage();
+	if (ctx.session.focusTimeout && ctx.session.focusInterval) {
+		clearTimeout(ctx.session.focusTimeout);
+		clearInterval(ctx.session.focusInterval);
+		delete ctx.session.focusTimeout;
+		delete ctx.session.focusInterval;
+		delete ctx.session.focusStarted;
+		return ctx.reply('Focus canceled.');
+	} else {
+		return ctx.reply('The focus has not started yet.');
+	}
+});
+
+bot.command('skip', async (ctx) => {
+	await ctx.deleteMessage();
+	if (ctx.session.breakTimeout && ctx.session.breakInterval) {
+		clearTimeout(ctx.session.breakTimeout);
+		clearInterval(ctx.session.breakInterval);
+		delete ctx.session.breakTimeout;
+		delete ctx.session.breakInterval;
+		delete ctx.session.focusStarted;
+		return ctx.reply('Break skiped.');
+	} else {
+		return ctx.reply('The break has not started yet.');
+	}
 });
 
 bot.command('playlist', async (ctx) => {
@@ -42,54 +65,17 @@ bot.command('playlist', async (ctx) => {
 });
 
 bot.action('startFocus', async (ctx) => {
-	if (ctx.session.focusState === 'busy') {
+	await db.createNewUser(ctx.from.id);
+	if (ctx.session.focusStarted) {
 		return ctx.answerCbQuery('Already started.');
 	}
-	ctx.session.focusState = 'busy';
-	const { focusPeriod, breakPeriod, todayStreak, dayGoal, currentDayStreak, bestDayStreak } = await db.getUserSettings(
-		ctx.from.id
-	);
-	await ctx.reply(`Focus started! (${focusPeriod}/${focusPeriod} min)`).then((data) => {
-		let timerValue = focusPeriod;
-		const timer = setInterval(async () => {
-			if (timerValue === 0) {
-				clearInterval(timer);
-				return;
-			}
-			await ctx.editMessageText(`Focus started! (${--timerValue}/${focusPeriod} min)`, {
-				message_id: data.message_id,
-			});
-		}, 60 * 1000);
-	});
+	ctx.session.focusStarted = true;
+	const userSettings = await db.getUserSettings(ctx.from.id);
 	await ctx.answerCbQuery('Focus!');
-
-	setTimeout(async () => {
-		await ctx.reply(`Focus finished! Have a break! (${breakPeriod}/${breakPeriod} min)`).then((data) => {
-			let timerValue = breakPeriod;
-			const timer = setInterval(async () => {
-				if (timerValue === 0) {
-					clearInterval(timer);
-					return;
-				}
-				await ctx.editMessageText(`Focus finished! Have a break! (${--timerValue}/${breakPeriod} min)`, {
-					message_id: data.message_id,
-				});
-			}, 60 * 1000);
-		});
-		await db.updateUserSettings(ctx.from.id, 'todayStreak', todayStreak + focusPeriod);
-		if (todayStreak + focusPeriod >= dayGoal) {
-			if (await db.completeDay(ctx.from.id)) {
-				await db.updateUserSettings(ctx.from.id, 'currentDayStreak', currentDayStreak + 1);
-				if (currentDayStreak + 1 > bestDayStreak) {
-					await db.updateUserSettings(ctx.from.id, 'bestDayStreak', currentDayStreak + 1);
-				}
-			}
-		}
-		setTimeout(async () => {
-			delete ctx.session.focusState;
-			return ctx.reply(`Break finished! Start a new focus session from the menu now!`);
-		}, breakPeriod * 60 * 1000);
-	}, focusPeriod * 60 * 1000);
+	await ctx.reply(`Focus started! (${userSettings.focusPeriod}/${userSettings.focusPeriod} min)`).then((data) => {
+		ctx.session.focusInterval = setFocusInterval(ctx, userSettings.focusPeriod, data.message_id);
+	});
+	ctx.session.focusTimeout = setfocusTimeout(ctx, userSettings);
 });
 
 bot.action('focusPeriod', async (ctx) => {
@@ -143,28 +129,6 @@ bot.action('closeMenu', async (ctx) => {
 	await ctx.deleteMessage();
 	await ctx.answerCbQuery('Menu closed!');
 });
-
-function showMenuKeyboard(ctx) {
-	return ctx.reply(
-		'Menu:',
-		Markup.inlineKeyboard([
-			[Markup.button.callback('Start focus session', 'startFocus')],
-			[
-				Markup.button.callback('Show settings', 'showSettings'),
-				Markup.button.callback('Useful days', 'showCompletedDays'),
-			],
-			[Markup.button.callback('Focus period', 'focusPeriod'), Markup.button.callback('Break period', 'breakPeriod')],
-			[Markup.button.callback('Day goal', 'dayGoal'), Markup.button.callback('Weekends', 'weekends')],
-			[Markup.button.callback('Close menu', 'closeMenu')],
-		])
-	);
-}
-
-async function changeSettingAction(ctx, setting, scene) {
-	ctx.session.settingToChange = setting;
-	await ctx.scene.enter(scene);
-	return ctx.answerCbQuery(setting);
-}
 
 if (process.env.LOCAL_MODE === 'on') {
 	bot.launch(() => console.log('Pomoboto bot is running locally.'));
---
+++ utils.js
@@ -0,0 +1,91 @@
+import { Markup } from 'telegraf';
+import db from './db.js';
+
+export const botCommands = [
+	{
+		command: '/start',
+		description: 'Open the bot menu',
+	},
+	{
+		command: '/cancel',
+		description: 'Cancel current focus session',
+	},
+	{
+		command: '/skip',
+		description: 'Skip the break timer',
+	},
+	{
+		command: '/playlist',
+		description: 'Spotify playlist',
+	},
+];
+
+export function showMenuKeyboard(ctx) {
+	return ctx.reply(
+		'Menu:',
+		Markup.inlineKeyboard([
+			[Markup.button.callback('Start focus session', 'startFocus')],
+			[
+				Markup.button.callback('Show settings', 'showSettings'),
+				Markup.button.callback('Useful days', 'showCompletedDays'),
+			],
+			[Markup.button.callback('Focus period', 'focusPeriod'), Markup.button.callback('Break period', 'breakPeriod')],
+			[Markup.button.callback('Day goal', 'dayGoal'), Markup.button.callback('Weekends', 'weekends')],
+			[Markup.button.callback('Close menu', 'closeMenu')],
+		])
+	);
+}
+
+export async function changeSettingAction(ctx, setting, scene) {
+	ctx.session.settingToChange = setting;
+	await ctx.scene.enter(scene);
+	return ctx.answerCbQuery(setting);
+}
+
+export function setFocusInterval(ctx, focusPeriod, messageId) {
+	let timerValue = focusPeriod;
+	return setInterval(async () => {
+		if (timerValue === 0) {
+			clearInterval(ctx.session.focusInterval);
+			delete ctx.session.focusInterval;
+			return;
+		}
+		await ctx.editMessageText(`Focus started! (${--timerValue}/${focusPeriod} min)`, {
+			message_id: messageId,
+		});
+	}, 60 * 1000);
+}
+
+export function setfocusTimeout(ctx, userSettings) {
+	const { focusPeriod, breakPeriod, todayStreak, dayGoal, currentDayStreak, bestDayStreak } = userSettings;
+	return setTimeout(async () => {
+		await ctx.reply(`Focus finished! Have a break! (${breakPeriod}/${breakPeriod} min)`).then((data) => {
+			let timerValue = breakPeriod;
+			ctx.session.breakInterval = setInterval(async () => {
+				if (timerValue === 0) {
+					clearInterval(ctx.session.breakInterval);
+					delete ctx.session.breakInterval;
+					return;
+				}
+				await ctx.editMessageText(`Focus finished! Have a break! (${--timerValue}/${breakPeriod} min)`, {
+					message_id: data.message_id,
+				});
+			}, 60 * 1000);
+		});
+		await db.updateUserSettings(ctx.from.id, 'todayStreak', todayStreak + focusPeriod);
+		if (todayStreak + focusPeriod >= dayGoal) {
+			if (await db.completeDay(ctx.from.id)) {
+				await db.updateUserSettings(ctx.from.id, 'currentDayStreak', currentDayStreak + 1);
+				if (currentDayStreak + 1 > bestDayStreak) {
+					await db.updateUserSettings(ctx.from.id, 'bestDayStreak', currentDayStreak + 1);
+				}
+			}
+		}
+		ctx.session.breakTimeout = setTimeout(async () => {
+			delete ctx.session.focusStarted;
+			delete ctx.session.focusTimeout;
+			delete ctx.session.breakTimeout;
+			return ctx.reply(`Break finished! Start a new focus session from the menu now!`);
+		}, breakPeriod * 60 * 1000);
+	}, focusPeriod * 60 * 1000);
+}
